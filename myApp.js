65488d7447038f7d1d776f65b89988b08447b6d3
Kashinath B.R <kashibr2004@gmail.com>
2025-08-16 23:17:27 +0530

    git commit -m "Enhance security headers with Helmet configuration"

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
 app.disable('x-powered-by');
 
 
